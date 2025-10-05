952862c5399e119ccd027f54f37cbb06d5060fa8
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-10-04 23:35:02 -0400

    Added Source/_Imports.ts.

---
+++ Source/_Imports.js
@@ -0,0 +1,6 @@
+"use strict";
+var bh = ThisCouldBeBetter.BitHandling;
+var Base64Encoder = bh.Base64Encoder;
+var BitStream = bh.BitStream;
+var ByteConverter = bh.ByteConverter;
+var ByteStream = bh.ByteStream;
---
+++ Source/_Imports.ts
@@ -0,0 +1,6 @@
+import bh = ThisCouldBeBetter.BitHandling;
+
+import Base64Encoder = bh.Base64Encoder;
+import BitStream = bh.BitStream;
+import ByteConverter = bh.ByteConverter;
+import ByteStream = bh.ByteStream;
